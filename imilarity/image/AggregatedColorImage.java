920ba4f74302f71fb01e25b4d9dda4eba9d7c914
klbostee <klbostee@071619a6-7901-0410-ab37-d954e9aca0b3>
2005-09-29 06:44:57 +0000

    AggregatedColorImage afgewerkt

--- imilarity/image/AggregatedColorImage.java
+++ imilarity/image/AggregatedColorImage.java
@@ -3,35 +3,58 @@
  */
 package image;
 
+import aggregators.Aggregator;
+
 /**
  * @author Klaas Bosteels
  */
 public class AggregatedColorImage implements ColorImage {
 
-	private ScalableColorImage[] images;
+	private ColorImage[] scaledImages;
+	private Aggregator aggregator;
+	
+	private static final int WIDTH = 100, HEIGHT = 100;
+	
+	public AggregatedColorImage(ScalableColorImage[] images, Aggregator aggregator) {
+		if (images == null)
+			throw new NullPointerException("images == null");
+		if (aggregator == null)
+			throw new NullPointerException("aggregator == null");
+		this.aggregator = aggregator;
+		scaledImages = new ColorImage[images.length];
+		for (int i = 0; i < images.length; i++)
+			scaledImages[i] = images[i].getScaledInstance(WIDTH,HEIGHT);
+	}
+	
 	
 	/**
 	 * @see image.ColorImage#getColorValues(int)
 	 */
 	public int[] getColorValues(int pixelNr) {
-		// TODO Auto-generated method stub
-		return null;
+		int values[][] = new int[3][scaledImages.length];
+		for (int i = 0; i < scaledImages.length; i++) {
+			int colorValues[] = scaledImages[i].getColorValues(pixelNr);
+			for (int j = 0; j < colorValues.length; j++)
+				values[j][i] = colorValues[j];
+		}
+		int result[] = new int[3];
+		for (int i = 0; i < result.length; i++)
+			result[i] = aggregator.aggregatedValue(values[i]);
+		return result;
 	}
 
 	/**
 	 * @see image.Image#getWidth()
 	 */
 	public int getWidth() {
-		// TODO Auto-generated method stub
-		return 0;
+		return WIDTH;
 	}
 
-	/* (non-Javadoc)
+	/**
 	 * @see image.Image#getHeight()
 	 */
 	public int getHeight() {
-		// TODO Auto-generated method stub
-		return 0;
+		return HEIGHT;
 	}
 
 }
