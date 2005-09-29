987568f27ef0e88e887e1245b6d91c1f4f38aae7
klbostee <klbostee@071619a6-7901-0410-ab37-d954e9aca0b3>
2005-09-29 06:46:06 +0000

    detail

--- imilarity/measures/ScalingGrayscaleMeasure.java
+++ imilarity/measures/ScalingGrayscaleMeasure.java
@@ -13,6 +13,8 @@ public class ScalingGrayscaleMeasure extends GrayscaleMeasureBase {
 
 	private GrayscaleMeasure measure;
 	
+	private static final int WIDTH = 100, HEIGHT = 100;
+	
 	public ScalingGrayscaleMeasure(GrayscaleMeasure measure) {
 		if (measure == null)
 			throw new NullPointerException("measure == null");
@@ -20,8 +22,8 @@ public class ScalingGrayscaleMeasure extends GrayscaleMeasureBase {
 	}
 	
 	public double similarity(ScalableGrayscaleImage gi1, ScalableGrayscaleImage gi2) {
-		ScalableGrayscaleImage sGi1 = gi1.getScaledInstance(100,100);
-		ScalableGrayscaleImage sGi2 = gi2.getScaledInstance(100,100);
+		ScalableGrayscaleImage sGi1 = gi1.getScaledInstance(WIDTH,HEIGHT);
+		ScalableGrayscaleImage sGi2 = gi2.getScaledInstance(WIDTH,HEIGHT);
 		return measure.similarity(sGi1,sGi2);
 	}
 	
