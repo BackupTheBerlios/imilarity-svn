eba362608419328b08b037c896ed981d7f9abd36
klbostee <klbostee@071619a6-7901-0410-ab37-d954e9aca0b3>
2005-10-24 21:19:24 +0000

    bugfix

--- Imilarity/de/berlios/imilarity/measures/M3.java
+++ Imilarity/de/berlios/imilarity/measures/M3.java
@@ -1,20 +1,22 @@
 package de.berlios.imilarity.measures;
 
+import de.berlios.imilarity.fuzzy.FuzzySet;
 import de.berlios.imilarity.fuzzy.Membership;
 
 
 public class M3 extends FuzzyMeasureBase {
 	
 	public double getSimilarity() {
-		double sum1 = 0.0, sum2 = 0.0;
-		int count = getQuery().getElementsCount();
+		double sum = 0.0;
+		FuzzySet a = getQuery();
+		FuzzySet b = getTarget();
+		int count = a.getElementsCount();
 		for (int i = 0; i < count; i++) {
-			Membership m1 = getQuery().getMembership(i);
-			Membership m2 = getTarget().getMembership(i);
-			sum1 += m1.minus(m2).abs();
-			sum2 += m1.plus(m2).abs();
+			Membership m1 = a.getMembership(i);
+			Membership m2 = b.getMembership(i);
+			sum += m1.minus(m2).abs();
 		}
-		return 1 - (sum1 / sum2);
+		return 1 - (sum / (a.abs() + b.abs()));
 	}
 
 
