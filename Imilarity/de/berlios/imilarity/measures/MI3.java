7c46c9b332d3822ddafacde4c65258d7ecef9b1a
klbostee <klbostee@071619a6-7901-0410-ab37-d954e9aca0b3>
2005-10-25 00:00:13 +0000

    bugfix

--- Imilarity/de/berlios/imilarity/measures/MI3.java
+++ Imilarity/de/berlios/imilarity/measures/MI3.java
@@ -7,7 +7,7 @@ public class MI3 extends FuzzyMeasureBase {
 	public double getSimilarity() {
 		FuzzySet a = getQuery();
 		FuzzySet b = getTarget();
-		return a.intersection(b).union(a.complement().intersection(b.complement())).abs() 
+		return a.intersection(b).intersection(a.complement().intersection(b.complement())).abs() 
 			/ a.union(b).intersection(a.complement().union(b.complement())).abs();
 	}
 
